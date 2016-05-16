4796d6c5f0f39de2fdaca1c6fcc5687cf5533cf7
Damian Fanaro <damianfanaro@gmail.com>
2016-05-16 15:31:26 -0300

    Testing

--- src/main/java/com/dfanaro/quickfixj/FIXApplication.java
+++ src/main/java/com/dfanaro/quickfixj/FIXApplication.java
@@ -19,20 +19,20 @@ public class FIXApplication implements Application {
 
     @Override
     public void fromAdmin(Message arg0, SessionID arg1) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
-        LOG.info("Successfully called fromAdmin for sessionId : " + arg0);
+//        LOG.info("Successfully called fromAdmin for sessionId : " + arg0);
     }
 
     @Override
     public void fromApp(Message message, SessionID sessionId) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
-        LOG.info("Successfully called fromApp for sessionId : " + message);
+//        LOG.info("Successfully called fromApp for sessionId : " + message);
 
 
 
-        if (message.getHeader().getField(new IntField(35)).equals("D")) {
+        if (message.getHeader().getField(new StringField(35)).getValue().equals("D")) {
             NewOrderSingle newOrder = (NewOrderSingle) message;
             ExecutionReport response = new ExecutionReport(new OrderID(newOrder.getRefOrderID().getValue()), new ExecID("1"), new ExecType('0'), new OrdStatus('0'), newOrder.getSide(), new LeavesQty(0), new CumQty(0));
             try {
-                LOG.info("Sending Execution Report");
+//                LOG.info("Sending Execution Report");
                 Session.sendToTarget(response, sessionId);
             }
             catch (SessionNotFound e ) {}
@@ -41,27 +41,27 @@ public class FIXApplication implements Application {
 
     @Override
     public void onCreate(SessionID arg0) {
-        LOG.info("Successfully called onCreate for sessionId : " + arg0);
+//        LOG.info("Successfully called onCreate for sessionId : " + arg0);
     }
 
     @Override
     public void onLogon(SessionID arg0) {
-        LOG.info("Successfully logged on for sessionId : " + arg0);
+//        LOG.info("Successfully logged on for sessionId : " + arg0);
     }
 
     @Override
     public void onLogout(SessionID arg0) {
-        LOG.info("Successfully logged out for sessionId : " + arg0);
+//        LOG.info("Successfully logged out for sessionId : " + arg0);
     }
 
     @Override
     public void toAdmin(Message message, SessionID sessionId) {
-        LOG.info("Inside toAdmin");
+//        LOG.info("Inside toAdmin");
     }
 
     @Override
     public void toApp(Message arg0, SessionID arg1) throws DoNotSend {
-        LOG.info("Message : " + arg0.toString().replace("\u0001", " | ") + " for sessionid : " + arg1);
+//        LOG.info("Message : " + arg0.toString().replace("\u0001", " | ") + " for sessionid : " + arg1);
     }
 
 //    public void onMessage(NewOrderSingle message, SessionID sessionID) throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
